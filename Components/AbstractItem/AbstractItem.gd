aefba2d2431ae76d23c7a28b341f828d03258830
Hicham12312 <153277101+Hicham12312@users.noreply.github.com>
2024-06-14 13:37:30 +0100

    s

--- Components/AbstractItem/AbstractItem.gd
+++ Components/AbstractItem/AbstractItem.gd
@@ -1,4 +1,6 @@
 extends Node2D
+class_name AbstractItem
+
 
 @onready var collision = $Area2D/CollisionShape2D
 @onready var sprite_2d = $Sprite2D
@@ -7,11 +9,6 @@ extends Node2D
 func _ready():
 	if stats != null:
 		sprite_2d.texture = stats.icon
-		if stats.name == Global.items_Dictionary.get("double_jump"):
-			return
-		if stats.icon.get_image().get_size() >= Vector2i(500,500):
-			sprite_2d.scale = Vector2(0.4, 0.4)
-			collision.shape.size = Vector2(150, 148)
 
 
 func _on_player_entered(body):
@@ -21,5 +18,3 @@ func _on_player_entered(body):
 	position = body.position
 	body.add_item(stats)
 	collision.call_deferred("set_disabled", true)
-	if Global.items_Dictionary.get("double_jump") == stats.name:
-		queue_free()
---
+++ Star.gd
@@ -0,0 +1,5 @@
+extends AbstractItem
+
+func _on_player_entered(body):
+	queue_free()
+
