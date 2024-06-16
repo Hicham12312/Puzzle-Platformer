57fb465cbc0b675193e45bb2c3b98ba2dbc8c3d4
Hicham12312 <153277101+Hicham12312@users.noreply.github.com>
2024-06-16 18:37:12 +0100

    jsjfoisdjfosfhsdoifjosd

--- Components/Singletons/scene_switcher.gd
+++ Components/Singletons/scene_switcher.gd
@@ -6,3 +6,6 @@ func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 	print_debug(current_scene)
+
+func switch_scene(res_path):
+	call_deferred("_deferred_goto_scene", res_path)
